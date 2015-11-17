0682152a85bb3bea84a5c8115057faba1c6cad5d
Selim Ok <selimok@gmail.com>
2015-11-17 23:47:54 +0100

    Create project base using Spring initializr

---
+++ src/main/java/com/example/SpringAngular2SeedApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngular2SeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAngular2SeedApplication.class, args);
+    }
+}
