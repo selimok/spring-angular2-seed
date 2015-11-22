698fcc86604d2a5d83213d6fed1fcd3c3de58eba
Selim Ok <selimok@gmail.com>
2015-11-22 18:42:11 +0100

    Add a simple spring controller and move generaterd javascript codes under app/generated

--- src/main/java/com/example/SpringAngular2SeedApplication.java
+++ src/main/java/com/example/SpringAngular2SeedApplication.java
@@ -2,6 +2,7 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 public class SpringAngular2SeedApplication {
@@ -9,4 +10,5 @@ public class SpringAngular2SeedApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringAngular2SeedApplication.class, args);
     }
+    
 }
